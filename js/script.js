1127cfee519600526db455e8e67e33b0beaeeb7c
Ancrom <remon90@list.ru>
2022-09-01 10:38:04 +0300

    add modal

--- js/script.js
+++ js/script.js
@@ -6,3 +6,18 @@ burger.addEventListener("click", () => {
   nav.classList.toggle("active");
   document.body.classList.toggle("no-scroll");
 });
+
+// =============== MODAL ===============
+const buttonClose = document.querySelector(".modal__button-close");
+const modal = document.querySelector(".modal");
+const buttonHero = document.querySelector(".hero__button");
+
+buttonHero.addEventListener("click", (e) => {
+  e.preventDefault();
+  modal.classList.add("active");
+
+  buttonClose.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.classList.remove("active");
+  });
+});
